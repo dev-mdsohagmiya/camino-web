dcf437e5be831d031c8d6e1237c6a17caa2f4aea
dev-mdsohagmiya <learnwithmsbhh@gmail.com>
2024-04-18 16:07:10 +0600

    updated footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -6,12 +6,12 @@ const Footer = () => {
   return (
     <footer className="bg-[#323232] text-white pt-[60px]">
       {/* info */}
-      <div className="max-w-[1482px] mx-auto px-7 grid grid-cols-12 gap-[30px]">
-        <div className="col-span-12 lg:col-span-6 flex gap-[30px] items-center justify-center lg:justify-start">
-          <h2 className="text-[30px] md:text-[64px] font-semibold">
+      <div className="max-w-[1482px] mx-auto px-7 grid grid-cols-12 gap-[5px] sm:gap-[30px]">
+        <div className="col-span-12 lg:col-span-6 flex gap-[15px] sm:gap-[30px] items-center justify-center lg:justify-start">
+          <h2 className="text-[27px] md:text-[64px] font-semibold">
             Let&apos;s discuss
           </h2>
-          <GoArrowUpRight className="text-[40px] md:text-[60px] border rounded-full p-[4px] md:p-[8px]" />
+          <GoArrowUpRight className="text-[32px] md:text-[60px] border rounded-full p-[4px] md:p-[8px]" />
         </div>
         <div className="col-span-12 lg:col-span-6 text-center lg:text-start text-[30px] md:text-[60px]">
           <span className="border-b border-solid border-white">
@@ -27,20 +27,28 @@ const Footer = () => {
       <div className="max-w-[1482px] mx-auto px-7 grid grid-cols-12 gap-[30px]">
         <div className="col-span-12 md:col-span-6 flex gap-[60px] justify-center md:justify-start">
           <div>
-            <p className="text-white opacity-50 uppercase">LOCAL TIME</p>
-            <h4 className="text-[22px] font-medium">2:14 PM</h4>
+            <p className="text-white opacity-50 uppercase  text-[14px] md:text-auto">
+              LOCAL TIME
+            </p>
+            <h4 className="text-[20px] md:text-[22px] font-medium">2:14 PM</h4>
           </div>
           <div>
-            <p className="text-white opacity-50 uppercase">WEATHER</p>
-            <h4 className="text-[22px] font-medium">1300 London</h4>
+            <p className="text-white opacity-50 uppercase text-[14px] md:text-auto">
+              WEATHER
+            </p>
+            <h4 className="text-[20px] md:text-[22px]  font-medium">
+              1300 London
+            </h4>
           </div>
         </div>
         <div className="col-span-12 md:col-span-6 text-center md:text-start">
-          <p className="text-white opacity-50 uppercase">Social</p>
-          <div className="flex gap-[15px] md:gap-[30px]">
-            <p className="text-[22px] font-medium">Instagram</p>
-            <p className="text-[22px] font-medium">Facebook</p>
-            <p className="text-[22px] font-medium">Instagram</p>
+          <p className="text-white opacity-50 uppercase text-[14px] md:text-auto">
+            Social
+          </p>
+          <div className="flex gap-[15px] md:gap-[30px] justify-center md:justify-start">
+            <p className="text-[20px] md:text-[22px]  font-medium">Instagram</p>
+            <p className="text-[20px] md:text-[22px]  font-medium">Facebook</p>
+            <p className="text-[20px] md:text-[22px]  font-medium">Instagram</p>
           </div>
         </div>
       </div>
